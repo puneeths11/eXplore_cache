439345e8a4efa44e42f3c77f05d8b068e8e2bc0a
puneeth <puneethspunii@gmail.com>
2023-12-26 16:26:32 +0530

    updated index.js

--- init/index.js
+++ init/index.js
@@ -1,28 +1,29 @@
+if (process.env.NODE_ENV != "production") {
+  require("dotenv").config();
+}
 const mongoose = require("mongoose");
-const initData = require("./data.js"); //Sample data
-const Listing = require("../models/listing.js"); //Schema
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/explorecache";
+const DB_URL = process.env.ATLASDB_URL;
 main()
   .then(() => {
-    console.log("Connecting to DB");
+    console.log("connected to DB");
   })
-  .catch((err) => {
-    console.log(err);
-  });
+  .catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect(MONGO_URL);
+  //   await mongoose.connect(MONGO_URL);
+  await mongoose.connect(DB_URL);
 }
 
 const initDB = async () => {
   await Listing.deleteMany({});
   initData.data = initData.data.map((obj) => ({
     ...obj,
-    owner: "657df2807971b05b498370cf",
+    owner: "658a98da9c34a6508e1c7b89",
   }));
   await Listing.insertMany(initData.data);
-  console.log("Data was initalized");
+  console.log("data was initialized");
 };
-
 initDB();
